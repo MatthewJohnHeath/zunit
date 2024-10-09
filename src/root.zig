c7122d0f241dd4967e96717d8a03afbb4e467d07
Mattthew Heath <matthew.john.heath@gmail.com>
2024-10-09 21:44:33 +0100

    same and before

--- src/root.zig
+++ src/root.zig
@@ -2,9 +2,8 @@ const std = @import("std");
 const testing = std.testing;
 const StructField = std.builtin.Type.StructField;
 
-
-fn i32Field(comptime fieldName:[:0]const u8) StructField{
-     return StructField{
+fn i32Field(comptime fieldName: [:0]const u8) StructField {
+    return StructField{
         .name = fieldName,
         .type = i32,
         .default_value = null,
@@ -13,83 +12,121 @@ fn i32Field(comptime fieldName:[:0]const u8) StructField{
     };
 }
 
-test "struct field is something"{
+test "struct field is something" {
     try testing.expect(i32Field("meter").name[0] == 'm');
 }
 
-fn BaseUnitPowerType(comptime name: [:0]const u8) type{
+fn BaseUnitPowerType(comptime name: [:0]const u8) type {
     return @Type(.{
         .Struct = .{
             .layout = std.builtin.Type.ContainerLayout.auto,
-            .fields = &[_] StructField{i32Field(name)},
+            .fields = &[_]StructField{i32Field(name)},
             .decls = &[_]std.builtin.Type.Declaration{},
             .is_tuple = false,
         },
     });
- }
+}
 
-test "can make meter power type"{
+test "can make meter power type" {
     try testing.expect(@hasField(BaseUnitPowerType("meter"), "meter"));
 }
- fn baseUnit(comptime name: [:0]const u8) BaseUnitPowerType(name){
-    comptime var unit:BaseUnitPowerType(name) = undefined;
+fn baseUnit(comptime name: [:0]const u8) BaseUnitPowerType(name) {
+    comptime var unit: BaseUnitPowerType(name) = undefined;
     @field(unit, name) = 1;
     return unit;
- }
+}
 
-test "can make meter"{
+test "can make meter" {
     try testing.expect(baseUnit("meter").meter == 1);
 }
 
-fn numberOfFieldsInCombined(comptime First : type, comptime Second : type) comptime_int{
+fn numberOfFieldsInCombined(comptime First: type, comptime Second: type) comptime_int {
     comptime var count = std.meta.fields(Second).len;
-        for(std.meta.fields(First)) |field|{
-        if(!@hasField(Second, field.name)){
+    for (std.meta.fields(First)) |field| {
+        if (!@hasField(Second, field.name)) {
             count = count + 1;
         }
         return count;
     }
 }
 
-test "numberOfFieldsInCombined different"{
-    const Meter  = BaseUnitPowerType("meter");
+test "numberOfFieldsInCombined different" {
+    const Meter = BaseUnitPowerType("meter");
     const Second = BaseUnitPowerType("second");
     try testing.expect(numberOfFieldsInCombined(Meter, Second) == 2);
 }
 
-test "numberOfFieldsInCombined same"{
-    const Meter  = BaseUnitPowerType("meter");
+test "numberOfFieldsInCombined same" {
+    const Meter = BaseUnitPowerType("meter");
     try testing.expect(numberOfFieldsInCombined(Meter, Meter) == 1);
 }
 
-fn MergeStruct(comptime First : type, comptime Second : type) type {
+fn before(comptime first: []const u8, comptime second: []const u8) bool {
+    const smaller_length = @min(first.len, second.len);
+    for (first[0..smaller_length], second[0..smaller_length]) |f, s| {
+        if (f < s) {
+            return true;
+        }
+        if (s < f) {
+            return false;
+        }
+    }
+    return first.len < second.len;
+}
+
+test "before" {
+    try testing.expect(before("aa", "ab"));
+    try testing.expect(!before("ab", "aa"));
+    try testing.expect(before("aa", "aaa"));
+    try testing.expect(!before("aa", "aa"));
+}
+
+fn same(comptime first: []const u8, comptime second: []const u8) bool {
+    if (first.len != second.len) {
+        return false;
+    }
+    for (first, second) |f, s| {
+        if (f != s) {
+            return false;
+        }
+    }
+    return true;
+}
+
+test "same" {
+    try testing.expect(same("foo", "foo"));
+    try testing.expect(!same("foo", "bar"));
+    try testing.expect(!same("foo", "foobar"));
+    try testing.expect(same("", ""));
+    try testing.expect(!same("foo", ""));
+}
+
+fn MergeStruct(comptime First: type, comptime Second: type) type {
     comptime var fields: [numberOfFieldsInCombined(First, Second)]StructField = undefined;
-    
-    for(std.meta.fields(First), 0..) |field, i|{
+
+    for (std.meta.fields(First), 0..) |field, i| {
         fields[i] = field;
     }
 
     comptime var i = std.meta.fields(First).len;
 
-    for(std.meta.fields(Second))|field|{
-        if(!@hasField(First, field.name)){
+    for (std.meta.fields(Second)) |field| {
+        if (!@hasField(First, field.name)) {
             fields[i] = field;
             i = i + 1;
         }
     }
-    
-    return @Type(.{
-        .Struct = .{
-            .layout = std.builtin.Type.ContainerLayout.auto,
-            .fields = &fields,
-            .decls = &[_]std.builtin.Type.Declaration{},
-            .is_tuple = false,
-        }
-    });
+
+    return @Type(.{ .Struct = .{
+        .layout = std.builtin.Type.ContainerLayout.auto,
+        .fields = &fields,
+        .decls = &[_]std.builtin.Type.Declaration{},
+        .is_tuple = false,
+    } });
 }
 
-test "MergeStruct different"{
-    const Meter  = BaseUnitPowerType("meter");
+test "MergeStruct different" {
+    const Meter = BaseUnitPowerType("meter");
     const Second = BaseUnitPowerType("second");
     const Combined = MergeStruct(Meter, Second);
 
@@ -98,8 +135,8 @@ test "MergeStruct different"{
     try testing.expect(@hasField(Combined, "second"));
 }
 
-test "MergeStruct same"{
-    const Meter  = BaseUnitPowerType("meter");
+test "MergeStruct same" {
+    const Meter = BaseUnitPowerType("meter");
     const Combined = MergeStruct(Meter, Meter);
 
     try testing.expect(std.meta.fields(Combined).len == 1);
