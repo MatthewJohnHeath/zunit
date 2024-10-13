bf471f703ff99e2421c174b9099d69203ce566f5
Mattthew Heath <matthew.john.heath@gmail.com>
2024-10-13 22:42:58 +0100

    fix comptime_fraction

--- src/comptime_fraction.zig
+++ src/comptime_fraction.zig
@@ -1,31 +1,33 @@
 const std = @import("std");
 const testing = std.testing;
 
-fn gcd(first: comptime_int, second : comptime_int) comptime_int{
+fn gcd(first: comptime_int, second: comptime_int) comptime_int {
     comptime var a = @abs(first);
     comptime var b = @abs(second);
-    while (a!=0){
-        (a,b) = (b%a, a);
+    while (a != 0) {
+        const temp = a;
+        a = b % a;
+        b = temp;
     }
     return b;
 }
 
 test "gcd" {
-    try testing.expect(gcd(6,4)  == 2);
-    try testing.expect(gcd(6,-5)  == 1);
-    try testing.expect(gcd(-6,-5)  == 1);
+    try testing.expect(gcd(6, 4) == 2);
+    try testing.expect(gcd(6, -5) == 1);
+    try testing.expect(gcd(-6, -5) == 1);
 }
 
-const ComptimeFraction{
-    numerator : comptime_int, 
-    denominator : comptime_int,
+const ComptimeFraction = struct {
+    numerator: comptime_int,
+    denominator: comptime_int,
     const Self = @This();
-    
-    pub fn init( top : comptime_int, bottom : comptime_int) Self{
+
+    pub fn init(top: comptime_int, bottom: comptime_int) Self {
         const divisor = gcd(top, bottom);
         comptime var numerator = top / divisor;
         comptime var denominator = bottom / divisor;
-        if(denominator < 0){
+        if (denominator < 0) {
             denominator = -denominator;
             numerator = -numerator;
         }
@@ -35,40 +37,37 @@ const ComptimeFraction{
         };
     }
 
-    pub fn neg(self:Self) Self{
-        return Self{ -numerator = -self.numerator, .denominator = self.denominator};
+    pub fn neg(self: Self) Self {
+        return Self{ .numerator = -self.numerator, .denominator = self.denominator };
     }
 
-    pub fn add(self: Self, other:Self) Self{
+    pub fn add(self: Self, other: Self) Self {
         return init(self.numerator * other.denominator + other.numerator * self.deonominator, self.denominator * other.denominator);
     }
 
-    pub fn sub(self: Self, other:Self) Self{
+    pub fn sub(self: Self, other: Self) Self {
         return self.add(other.neg());
     }
 
-    pub fn reciprocal(self:Self) Self{
-        if(self.numerator >= 0){
-            return Self {.numerator = self.denominator, .denominator = self.numerator};
+    pub fn reciprocal(self: Self) Self {
+        if (self.numerator >= 0) {
+            return Self{ .numerator = self.denominator, .denominator = self.numerator };
+        } else {
+            return Self{ .numerator = -self.denominator, .denominator = -self.numerator };
         }
-        else{return Self {.numerator = -self.denominator, .denominator = -self.numerator}; }
-        
     }
 
-    pub fn mul(self: Self, other:Self) Self{
+    pub fn mul(self: Self, other: Self) Self {
         return init(self.numerator * other.numerator, self.denominator * other.denominator);
     }
 
-    pub fn div(self: Self, other:Self) Self{
+    pub fn div(self: Self, other: Self) Self {
         return self.mul(other.reciprocal());
     }
 
-    pub fn to_float(comptime self:Self) comptime_float{
-        const var numerator : comptime_float = @floatFromInt(self.numerator);
-        const var denominator : comptime_float = @floatFromInt(self.denominator);
-        retirn numerator / denominator;
+    pub fn to_float(comptime self: Self) comptime_float {
+        const numerator: comptime_float = @floatFromInt(self.numerator);
+        const denominator: comptime_float = @floatFromInt(self.denominator);
+        return numerator / denominator;
     }
-}
-test "is this found?" {
-    try testing.expect(false);
-}
+};
