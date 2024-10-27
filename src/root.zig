b65dff059621e406b2a9992a237c133fb78bfa81
Mattthew Heath <matthew.john.heath@gmail.com>
2024-10-27 15:30:03 +0000

    SI base units

--- src/root.zig
+++ src/root.zig
@@ -9,5 +9,53 @@ pub fn Metre(Scalar: type) type {
 }
 
 test "Metre" {
-    try testing.expect((Metre(f16) == Units(f16).BaseQuantity("metre")));
+    try testing.expect(Metre(f16) == Units(f16).BaseQuantity("metre"));
+}
+
+pub fn Second(Scalar: type) type {
+    return Units(Scalar).BaseQuantity("second");
+}
+
+test "Second" {
+    try testing.expect(Second(f16) == Units(f16).BaseQuantity("second"));
+}
+
+pub fn Kilogram(Scalar: type) type {
+    return Units(Scalar).BaseQuantity("kilogram");
+}
+
+test "Kilogram" {
+    try testing.expect(Kilogram(f16) == Units(f16).BaseQuantity("kilogram"));
+}
+
+pub fn Ampere(Scalar: type) type {
+    return Units(Scalar).BaseQuantity("ampere");
+}
+
+test "Ampere" {
+    try testing.expect(Ampere(f16) == Units(f16).BaseQuantity("ampere"));
+}
+
+pub fn Kelvin(Scalar: type) type {
+    return Units(Scalar).BaseQuantity("kelvin");
+}
+
+test "Kelvin" {
+    try testing.expect(Kelvin(f16) == Units(f16).BaseQuantity("kelvin"));
+}
+
+pub fn Mole(Scalar: type) type {
+    return Units(Scalar).BaseQuantity("mole");
+}
+
+test "Mole" {
+    try testing.expect(Mole(f16) == Units(f16).BaseQuantity("mole"));
+}
+
+pub fn Candela(Scalar: type) type {
+    return Units(Scalar).BaseQuantity("candela");
+}
+
+test "Candela" {
+    try testing.expect(Candela(f16) == Units(f16).BaseQuantity("candela"));
 }
