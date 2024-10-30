8c5e8367981af3ad06b4550f8eed88dca4f7621f
Matthew Heath <matthew.john.heath@gmail.com>
2024-10-30 18:38:19 +0000

    Offset quantity functions

---
+++ src/offset_quantity.zig
@@ -0,0 +1,88 @@
+const std = @import("std");
+const testing = std.testing;
+//const quantity = @import("quantity.zig");
+const fraction = @import("comptime_fraction.zig");
+
+const Fraction = fraction.ComptimeFraction;
+
+pub fn OffsetUnit(AbsoluteUnit: type, offset : Fraction)type{
+    if offset.isZero() {
+        return AbsoluteUnit;
+    }
+    return struct{
+        pub fn Of(Scalar: type) type{
+            const Outer = @This();
+            return struct{
+                value : Scalar,
+
+                const Self = @This();
+                const UnitType = Outer;
+
+                fn assertSameUnits(other: anytype, comptime function_name: []const u8) void {
+                    if ( UnitType != @TypeOf(other).UnitType) {
+                        @compileError("It is not permitted to call " ++ function_name ++ " except on OffsetUnit types with the same units");
+                    }
+                }
+
+                pub fn eql(self: Self, other: anytype) bool {
+                    assertSameUnits(other, "eql");
+                    return self.value == other.value;
+                }
+
+                pub fn neql(self: Self, other: anytype) bool {
+                    assertSameUnits(other, "neql");
+                    return !self.eql(other);
+                }
+
+                pub fn lt(self: Self, other: anytype) bool {
+                    assertSameUnits(other, "lt");
+                    return self.value < other.value;
+                }
+
+                pub fn gt(self: Self, other: anytype) bool {
+                    assertSameUnits(other, "gt");
+                    return other.lt(self);
+                }
+
+                pub fn le(self: Self, other: anytype) bool {
+                    assertSameUnits(other, "le");
+                    return !self.gt(other);
+                }
+
+                pub fn ge(self: Self, other: anytype) bool {
+                    assertSameUnits(other, "ge");
+                    return !self.lt(other);
+                }
+
+                fn TranslatedType(TranslationType : type) type{
+                    if(AbsoluteUnit != TranslationType.UnitType){
+                        @compileError("add and sub for OffsetUnit quantities can only be called on quantities of the underlying type.");
+                    }
+                    const self: Self = undefined;
+                    const other: TranslationType = undefined;
+                    return Of(@TypeOf(self.value, other.value));
+                }
+
+                pub fn add(self : Self, quantity : anytype) TranslatedType(@TypeOf(quantity)){
+                    return .{.value = self.value + quantity.value};
+                }
+
+                pub fn sub(self : Self, quantity : anytype) TranslatedType(@TypeOf(quantity)){
+                    return .{.value = self.value - quantity.value};
+                }
+
+                fn DifferenceType(OtherType : type) type{
+                    if ( UnitType != OtherType.UnitType) {
+                        @compileError("diff can only be called on OffsetUnit types with the same units");
+                    }
+                    const self: Self = undefined;
+                    const other: TranslationType = undefined;
+                    return AbsoluteUnit.Of(@TypeOf(self.value, other.value));
+                }
+                pub fn diff(self: Self, other: anytype) DifferenceType(@TypeOf(other)){
+                    return .{.value = self.value - other.value};
+                }
+            }
+        }
+    };
+}
